0df71f86cde8de8303ca8e5f885fce319ee5a879
zkingboos <kingboosoficial@gmail.com>
2021-01-11 21:07:03 -0300

    working with git submodules

--- engine/src/main/java/io/github/aluria/engine/gson/type/TypeAware.java
+++
@@ -1,7 +0,0 @@
-package io.github.aluria.engine.gson.type;
-
-import com.google.gson.reflect.TypeToken;
-
-public final class TypeAware<T> extends TypeToken<T> {
-
-}
--- kingdoms/src/main/java/io/github/aluria/kingdoms/validators/Validator.java
+++
@@ -1,6 +0,0 @@
-package io.github.aluria.kingdoms.validators;
-
-public interface Validator<T> {
-
-    boolean validate(T value);
-}
